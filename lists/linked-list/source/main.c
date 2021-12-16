860ef109fdddb7c6dae5dfb1233ee07576753cc4
Weydans Barros <weydans@hotmail.com>
2021-12-16 15:13:23 -0300

    Correcting .gitignore, removing .*.swp file and adding comments on main.c

--- lists/linked-list/source/main.c
+++ lists/linked-list/source/main.c
@@ -1,9 +1,24 @@
+/* **************************************************************************
+main.c
+
+Explane how to use linked list
+
+Author: Weydans Barros
+Date:	16/12/2021
+
+Compile:
+gcc main.c -o maini
+
+************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/* *********************************************************************** */
+
 int main ( int argc, char* argv[] )
 {
-	
+	printf("Running!!!\n");	
 
 	return 0;
 }
