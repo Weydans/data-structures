6e1d57b2e91c816d0af863a0cb3e688fa7ff3535
Weydans <weydans@hotmail.com>
2021-12-16 16:27:17 -0300

    Creation test.c file on linked list

---
+++ lists/linked-list/test/test.c
@@ -0,0 +1,22 @@
+/* **************************************************************************
+test.c
+
+Responsible to chech linked list functions
+
+Author: Weydans Barros
+Date:	16/12/2021
+
+Compile:
+gcc test/test.c -o test 
+
+************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* *********************************************************************** */
+
+int main ( int argc, char* argv[] )
+{
+	return 0;
+}
