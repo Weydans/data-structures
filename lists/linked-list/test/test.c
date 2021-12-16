95162750f3cf5b5cce604eeba8cca854294e5f5c
Weydans <weydans@hotmail.com>
2021-12-16 16:41:38 -0300

    Starting tests with TDD and changing compilling output name

--- lists/linked-list/test/test.c
+++ lists/linked-list/test/test.c
@@ -7,16 +7,19 @@ Author: Weydans Barros
 Date:	16/12/2021
 
 Compile:
-gcc test/test.c -o test 
+gcc -I./include test/test.c -o test-linked-list 
 
 ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <linked-list.h>
 
 /* *********************************************************************** */
 
 int main ( int argc, char* argv[] )
 {
+	LINKLED_LIST* list = linked_list_new();
+
 	return 0;
 }
