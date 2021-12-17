0efd8f8af3db94c4537c53f50597117e454038e1
Weydans <weydans@hotmail.com>
2021-12-17 18:19:37 -0300

    Creating interface to get linked-list length and add item, mapping possible exceptions in those functions

--- lists/linked-list/include/linked-list.h
+++ lists/linked-list/include/linked-list.h
@@ -1,8 +1,14 @@
 #ifndef _LINKED_LIST_H_
 #define _LINKED_LIST_H_
 
+#define NULL_POINTER					-1
+#define INVALID_ITEM					-2
+#define ITEM_MEMMORY_ALLOCATION_ERROR	-3
+
 typedef struct LINKLED_LIST LINKLED_LIST;
 
 LINKLED_LIST* linked_list_new ();
+int linked_list_length ( LINKLED_LIST* list);
+int linked_list_add ( LINKLED_LIST* list, void* item );
 
 #endif
