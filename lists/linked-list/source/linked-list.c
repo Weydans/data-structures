3787b7159cc5bf867ad6f748b9f91680fd6c926e
Weydans <weydans@hotmail.com>
2021-12-17 18:21:32 -0300

    Implementing struct NODE, Updating struct LINKED_LIST, Implementing functions to add item and get length of a list

--- lists/linked-list/source/linked-list.c
+++ lists/linked-list/source/linked-list.c
@@ -1,16 +1,52 @@
 #include <stdlib.h> 
 #include "linked-list.h" 
 
+typedef struct NODE NODE;
+
+struct NODE {
+	void* data;
+	NODE* next;
+};
+
 struct LINKLED_LIST {
-	size_t length;
+	int length;
+	NODE* head;
 };
 
 LINKLED_LIST* linked_list_new ()
 {
 	LINKLED_LIST* list = ( LINKLED_LIST* ) calloc( 1, sizeof( LINKLED_LIST ) ); 
 
+	if ( !list ) return NULL;
+
 	list->length = 0;
+	list->head 	 = NULL;
 
 	return list;
 }
 
+int linked_list_length ( LINKLED_LIST* list )
+{
+	if ( !list ) return NULL_POINTER;
+	return list->length;
+}
+
+int linked_list_add( LINKLED_LIST* list, void* item )
+{
+	if ( !list ) return NULL_POINTER;
+	if ( !item ) return INVALID_ITEM;
+
+	if ( list->length == 0 ) 
+	{
+		list->head = (NODE*) calloc( 1, sizeof( NODE ) );
+	
+		if ( !list->head ) return ITEM_MEMMORY_ALLOCATION_ERROR;
+
+		list->head->data = item;
+		list->head->next = NULL;
+
+		list->length++;
+	}
+
+	return 0;
+}
