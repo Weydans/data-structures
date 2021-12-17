e27fec7030b8db5c67896ccbf5782694187fe158
Weydans <weydans@hotmail.com>
2021-12-17 16:48:05 -0300

    Testing creation of linked-list

--- lists/linked-list/test/test.c
+++ lists/linked-list/test/test.c
@@ -17,9 +17,28 @@ gcc -I./include source/linked-list.c test/test.c -o test-linked-list
 
 /* *********************************************************************** */
 
+#define ERROR(m,c) {\
+			printf("\033[31;01m%s:%d: %s\n\033[00m", __FILE__, __LINE__, m);\
+			exit(c);\
+		}
+
+void test_linked_list_new ( LINKLED_LIST* list );
+
+/* *********************************************************************** */
+
 int main ( int argc, char* argv[] )
 {
 	LINKLED_LIST* list = linked_list_new();
 
+	test_linked_list_new( list );
+
 	return 0;
 }
+
+/* *********************************************************************** */
+
+void test_linked_list_new ( LINKLED_LIST* list )
+{
+	if ( !list ) ERROR("Fail to create 'linked-list'", -1);		
+}
+
